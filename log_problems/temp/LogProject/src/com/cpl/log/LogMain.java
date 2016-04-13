e01cb9ddd8ba04f61e24a0ed986afbb89eacec33
zartkims <sycaipengli@gmail.com>
2016-04-14 00:24:19 +0800

    1.直接花了半个小时写了个程序处理文档

---
+++ log_problems/temp/LogProject/src/com/cpl/log/LogMain.java
@@ -0,0 +1,97 @@
+package com.cpl.log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class LogMain {
+	public static int sWhollyPrint = 50;//如果比这个多全都打出来
+	public static void main(String[] args) throws Exception {
+//		System.out.println(new File("log_01.log").exists());
+		try {
+			//传递的参数第一个是检测大于多少则输出  第二个参数则是大于多少则输出全部log默认50
+			int bigThan = 0;
+			if (null != args && args.length > 0) {
+				bigThan = getInt(args[0], 0);
+				if (args.length > 1) {
+					sWhollyPrint = getInt(args[1], 50);
+				}
+			}
+			
+			File file = new File("log_01.log");
+			File file2 = new File("log_02.log");
+			File outFile = new File("result1"+"_bigthan_"+bigThan);
+			File outFile2 = new File("result2"+"_bigthan_"+bigThan);
+			
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+			
+			dealFile(reader, writer, bigThan);
+			
+			reader = new BufferedReader(new FileReader(file2));
+			writer = new BufferedWriter(new FileWriter(outFile2));
+			dealFile(reader, writer, bigThan);
+	        System.out.println("finish");
+		} catch (Exception e) {
+			System.err.print("请确保jar和log文件在同一目录下,并且第一个log文件命名为log_01.log,第二个命名为log_02.log");
+			System.out.println(e);
+		}
+		
+	}
+	
+	private static Integer getInt(String num, int def) {
+		try {
+			int res = Integer.valueOf(num.trim());
+			return res;
+		} catch (Exception e) {
+			return def;
+		}
+	}
+
+	public static void dealFile(BufferedReader reader, BufferedWriter writer, int big) throws Exception {
+		try {
+			String line;
+			boolean findFirstUs = false;//我们的栈的第一个log也就是直接调用那个log
+			boolean whollyPrint = false;
+			while ((line = reader.readLine()) != null) {
+				if (line.contains("StrictMode policy violation; ~duration=")) {
+					whollyPrint = false;
+					int numin = line.indexOf('=') + 1;
+					int afterP = line.indexOf("ms: android");
+					String time = line.substring(numin, afterP);
+					if (getInt(time, 0) > big) {
+						if(getInt(time, 0) > sWhollyPrint) {
+							whollyPrint = true; 
+						}
+						writer.write("\r\n\r\n===================\r\n");
+						writer.write("cost time : " + time + "\r\n");
+						writer.write(line+ "\r\n");
+						line = reader.readLine();
+						writer.write(line+ "\r\n");//这一行是类型信息
+						findFirstUs = true;
+					}
+				}
+				if (whollyPrint) {
+					findFirstUs = false;
+					writer.write(line+"\r\n");
+				} else if (findFirstUs) {
+					if (line.contains("at com.sogou.")) {
+						findFirstUs = false;
+						writer.write(line+"\r\n");
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			throw e;
+		} finally {
+			reader.close();
+			writer.close();
+		}
+		
+	}
+}
